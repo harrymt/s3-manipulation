3ddec0e3114ff71d486397598930fac8c8f69b53
Harry Mumford-Turner <harrymt@me.com>
2018-04-20 13:52:31 +0100

    Added basic S3 Config

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import { S3, config, SharedIniFileCredentials } from "aws-sdk";
+
+// Load the SDK for JavaScript
+const setupAWSConfig = (region = "us-west-2", profile = "default") => {
+    config.update({ region });
+    config.credentials = new SharedIniFileCredentials({ profile });
+};
+
+const s3 = new S3({apiVersion: "2006-03-01"});
